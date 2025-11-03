40b63bc2492bbd33acee1da46ffde0698db52654
micahtilton <micah.tilton.20@gmail.com>
2025-11-03 01:13:15 -1000

    added fastapi endpoint

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -1,7 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+parent_folder = Path(__file__).parent.parent
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file="../.env")
+    model_config = SettingsConfigDict(env_file=parent_folder / ".env")
     openai_api_key: str
     openai_llm: str
     lancedb_storage_path: str
---
+++ backend/main.py
@@ -0,0 +1,14 @@
+from fastapi import Body, Depends, FastAPI
+
+from backend.api.application import DegreePathwayPredictor, get_degree_pathway_predictor
+from backend.api.models import CompleteDegreePathway
+
+app = FastAPI()
+
+
+@app.post("/predict", response_model=CompleteDegreePathway)
+def predict_degree_pathway(
+    query: str = Body(...),
+    predictor: DegreePathwayPredictor = Depends(get_degree_pathway_predictor),
+) -> CompleteDegreePathway:
+    return predictor.predict(query)
